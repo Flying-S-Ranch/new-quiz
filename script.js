95e48d084a2dda48ec093036e16cb53540b13fd0
Samantha <samleashrauner@gmail.com>
2023-10-09 12:13:21 -0500

    pseudo-code script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// DEPENDENCIES
+// start-button
+
+
+// DATA / STATE
+// start-box
+
+// quiz-box
+
+// score-box
+
+
+// FUNCTIONS
+
+
+// USER INTERACTIONS
+// user clicks the start button
+    // start box stops displaying
+    // quiz box appears
+    // timer starts
+    // first question displayed
+
+// user chooses an answer
+    // if the answer is incorrect, ten seconds are deducted from the clock
+    // the quiz moves on to the next question
+
+// user enters their initials
+    // initials and score are saved to local storage
+    // initials and score are displayed with other scores
+
+
+// INITIALIZATIONS
