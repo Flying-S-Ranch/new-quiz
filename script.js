138510877c48fc3c8c6e965d8eee0cfbeab86a03
Samantha <samleashrauner@gmail.com>
2023-10-11 09:21:33 -0500

    style the startBox div

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var body = document.body;
 var startBox = document.createElement("div")
 var startButton = document.createElement("button")
 
-startBox.textContent = "Hello World"
+startBox.textContent = "Ready to test your JavaScript knowledge?"
 startButton.textContent = "Start the Quiz"
 
 body.appendChild(startBox)
 startBox.appendChild(startButton)
 
-startBox.setAttribute("style", "margin:auto; width: 50%; text-align:center")
+startBox.setAttribute("style", "margin:auto; width: 50%; text-align:center; padding-top: 15px")
+startButton.setAttribute("style", "width: 20%; background-color: teal; margin: auto")
