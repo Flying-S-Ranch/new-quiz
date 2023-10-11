7276a642ff197f86db5d79c1ffd2de4fec20083c
Samantha <samleashrauner@gmail.com>
2023-10-11 08:49:25 -0500

    adds trial h1 element

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+var body = document.body;
+var h1El = document.createElement("h1")
 
+h1El.textContent = "Hello World"
 
+body.appendChild(h1El)
+
+h1El.setAttribute("style", "margin:auto; width: 50%; text-align:center")
