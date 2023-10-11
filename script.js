ae92aa430fbbac37ee509a4c55e32e737649d8a7
Samantha <samleashrauner@gmail.com>
2023-10-11 11:51:38 -0500

    adds basic functionality to the quiz box

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 // DEPENDENCIES
 var body = document.body;
+// this is the box for the start of the quiz
 var startBox = document.createElement("div")
 var startText = document.createElement("h2")
 var startButton = document.createElement("button")
-
 var startDisplay = "visible"
 
+// this is the box that displays the questions
+var quizBox = document.createElement("div")
+var questionText = document.createElement("h3")
+var ansA = document.createElement("button")
+var ansB = document.createElement("button")
+var ansC = document.createElement("button")
+var ansD = document.createElement("button")
+var quizDisplay = "none"
+
 // DATA / STATE
+// text of the start box
 startText.textContent = "Ready to test your JavaScript knowledge?"
 startButton.textContent = "Start the Quiz"
 
+// text of the quiz box
+questionText.textContent = "Hello World"
+
+// children of the start box
 body.appendChild(startBox)
 startBox.appendChild(startText)
 startBox.appendChild(startButton)
 
+// children of the quiz box
+body.appendChild(quizBox)
+quizBox.appendChild(questionText)
+
+
+// attributes of the start box
 startBox.setAttribute("id", "start-box")
 startBox.setAttribute("class", "display-visible")
 startButton.setAttribute("style", "width: 20%; background-color: teal; margin: auto; padding: 5px; border-radius: 3px")
+
+// attributes of the quiz box
+quizBox.setAttribute("class", "display-none")
 
 // FUNCTIONS
 
@@ -26,6 +49,8 @@ startBox.addEventListener("click", function() {
     if (startDisplay === "visible") {
         startDisplay = "none";
         startBox.setAttribute("class","display-none")
+        quizDisplay = "visible"
+        quizBox.setAttribute("class", "display-visible")
     }
 })
 
