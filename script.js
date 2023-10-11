bd1c4ce02239966143738841b1efd43675fefee6
Samantha <samleashrauner@gmail.com>
2023-10-11 09:27:29 -0500

    adds h2 element to startBox div

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var body = document.body;
 var startBox = document.createElement("div")
+var startText = document.createElement("h2")
 var startButton = document.createElement("button")
 
-startBox.textContent = "Ready to test your JavaScript knowledge?"
+startText.textContent = "Ready to test your JavaScript knowledge?"
 startButton.textContent = "Start the Quiz"
 
 body.appendChild(startBox)
+startBox.appendChild(startText)
 startBox.appendChild(startButton)
 
 startBox.setAttribute("style", "margin:auto; width: 50%; text-align:center; padding-top: 15px")
-startButton.setAttribute("style", "width: 20%; background-color: teal; margin: auto")
+startButton.setAttribute("style", "width: 20%; background-color: teal; margin: auto; padding: 5px; border-radius: 3px")
