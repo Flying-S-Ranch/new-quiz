c32a8ff1e1f2859060f1626ecba499b3b52f9d66
Samantha <samleashrauner@gmail.com>
2023-10-11 12:07:51 -0500

    style quiz box

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var startDisplay = "visible"
 // this is the box that displays the questions
 var quizBox = document.createElement("div")
 var questionText = document.createElement("h3")
+var answerDiv = document.createElement("div")
 var ansA = document.createElement("button")
 var ansB = document.createElement("button")
 var ansC = document.createElement("button")
@@ -22,6 +23,10 @@ startButton.textContent = "Start the Quiz"
 
 // text of the quiz box
 questionText.textContent = "Hello World"
+ansA.textContent = "Hello A"
+ansB.textContent = "Hello B"
+ansC.textContent = "Hello C"
+ansD.textContent = "Hello D"
 
 // children of the start box
 body.appendChild(startBox)
@@ -31,6 +36,11 @@ startBox.appendChild(startButton)
 // children of the quiz box
 body.appendChild(quizBox)
 quizBox.appendChild(questionText)
+quizBox.appendChild(answerDiv)
+answerDiv.appendChild(ansA)
+answerDiv.appendChild(ansB)
+answerDiv.appendChild(ansC)
+answerDiv.appendChild(ansD)
 
 
 // attributes of the start box
@@ -40,7 +50,12 @@ startButton.setAttribute("style", "width: 20%; background-color: teal; margin: a
 
 // attributes of the quiz box
 quizBox.setAttribute("class", "display-none")
-
+quizBox.setAttribute("id", "quiz-box")
+answerDiv.setAttribute("id", "answer-div")
+ansA.setAttribute("class", "answer-button")
+ansB.setAttribute("class", "answer-button")
+ansC.setAttribute("class", "answer-button")
+ansD.setAttribute("class", "answer-button")
 // FUNCTIONS
 
 
